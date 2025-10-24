3bb33a48882e060e9a8819f68113ac2b5ec71f7a
Inés Galdeano <igaldeano@mail.austral.edu.ar>
2025-10-24 10:22:00 -0300

    Documentacion de suma

--- R/suma.R
+++ R/suma.R
@@ -1,3 +1,16 @@
+
+#' Suma de dos números
+#'
+#' Suma dos numeros que deben ser positivos
+#' @param x Un valor o vector numérico
+#' @param y Un valor o vector numérico
+#'
+#' @returns
+#' Un valor o vector numérico con la suma de x + y
+#' @examples
+#' suma(10,2)
+#' suma (3, 1)
+#' @export
 suma <- function(x,y){
   x+y
 }
